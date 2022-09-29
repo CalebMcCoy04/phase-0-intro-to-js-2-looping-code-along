c84872f307e7fef941381b7e41f17ab240c1405e
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-09-28 19:24:10 -0500

    commit tree f3fe4b0ff41e14f265116eac713288173b05febc

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solutions in this file
+/*
+for ([initialization]; [condition]; [iteration]) {
+    [loop body]
+  };
+*/
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+};
+*/
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+const myNames = ["Guadalupe", "Ollie", "Aki"];
+const myEvent = "graduation"
+
+
+
+function writeCards(arrayOfNames, event){
+    const newArray = [];
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        newArray.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`);
+        
+    }
+    return newArray
+};
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday")
+writeCards(myNames, myEvent);
+
+
+function countDown(number) {
+    let myNumber = 10;
+    while (myNumber > -1){
+        console.log(myNumber--);
+    }
+}
